0abc0a011fa43d541c8e2f7d142027ca91fcf945
manug16 <manu@ManuMacBook.local>
2014-12-03 16:37:51 +0000

    Lab09 Manu Gualandri
    Ready to hand in

--- Lab09.java
+++ Lab09.java
@@ -2,8 +2,8 @@
  * A robot moves along a row, collecting piles of beepers and placing them one square to
  * the right.
  * 
- * @author <...>
- * @version <...>
+ * @author <Manu Gualandri>
+ * @version <25/11/2014>
  *
  */
 
@@ -11,13 +11,82 @@ import edu.fcps.karel2.Display;
 import javax.swing.JOptionPane;
 
 public class Lab09 {
-	 
+    
 	 public static void main(String[] args) {
 		 String map = JOptionPane.showInputDialog("Which map?");
 		 Display.openWorld("maps/"+map+".map");
 		 Display.setSize(10, 10);
-		 Athlete athena = new Athlete(1, 1, Display.EAST, 0);
-		 //TODO write a combination of definite and indefinite loops that will cause athena
-		 // to pick up each pile of beepers and deposit them one square to the right.
+		 Display.setSpeed(10);
+       Athlete athena = new Athlete(1, 1, Display.EAST, 0);
+       int beepers = 0;
+       while(athena.nextToABeeper()) {
+            athena.pickBeeper();
+            beepers++;
+       }
+       athena.move();
+       int p = 0;
+       while(athena.nextToABeeper()) {
+            athena.pickBeeper();
+            p++;
+       }
+       for(int n = beepers; n > 0; n--) {
+            athena.putBeeper();
+            beepers--;
+       }
+       athena.move();
+       while(athena.nextToABeeper()) {
+            athena.pickBeeper();
+            beepers++;
+       }
+       for(int z = p; z > 0; z--) {
+            athena.putBeeper();
+            p--;
+       }
+       athena.move();
+       while(athena.nextToABeeper()) {
+            athena.pickBeeper();
+            p++;
+       }
+       for(int n = beepers; n > 0; n--) {
+            athena.putBeeper();
+            beepers--;
+       }
+       athena.move();
+       while(athena.nextToABeeper()) {
+            athena.pickBeeper();
+            beepers++;
+       }
+       for(int z = p; z > 0; z--) {
+            athena.putBeeper();
+            p--;
+       }
+       athena.move();
+       while(athena.nextToABeeper()) {
+            athena.pickBeeper();
+            p++;
+       }
+       for(int n = beepers; n > 0; n--) {
+            athena.putBeeper();
+            beepers--;
+       }
+       athena.move();
+       while(athena.nextToABeeper()) {
+            athena.pickBeeper();
+            beepers++;
+       }
+       for(int z = p; z > 0; z--) {
+            athena.putBeeper();
+            p--;
+       }
+       athena.move();
+       while(athena.nextToABeeper()) {
+            athena.pickBeeper();
+            p++;
+       }
+       for(int n = beepers; n > 0; n--) {
+            athena.putBeeper();
+            beepers--;
+       }
+       athena.move();
 	 }
  }
